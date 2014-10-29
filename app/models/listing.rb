41e1c7afa13254af7384e8b3f3d1df64add9415a
Ant <alonezuko@mail.ru>
2014-10-29 16:22:42 +0400

    Install and set up Devise gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,13 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
-    else
+	#if Rails.env.development?
+		#has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+   # else
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
  	                               :storage => :dropbox,
                                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-								   :path => ":style/:id_:filename"
- 	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png) #проверка контента(картинки) на соответствие формату
-	end
-end
+								   #:path => ":style/:id_:filename"
+
+ 	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)  # проверка контента(картинки) на соответствие формату
+  end
+#end
+
