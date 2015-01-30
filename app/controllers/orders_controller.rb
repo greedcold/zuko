7749b47f756445025dcc4014fd76d82ecbde2672
Ant <alonezuko@mail.ru>
2015-01-30 21:35:50 +0300

    reorganize Order links and url

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,22 +10,10 @@ class OrdersController < ApplicationController
     @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
-  end
-
-  def show
-    respond_with(@order)
-  end
-
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
     respond_with(@order)
-  end
-
-  def edit
   end
 
   def create
@@ -38,16 +26,6 @@ class OrdersController < ApplicationController
 
     @order.buyer_id = current_user.id
     @order.save
-    respond_with(@order)
-  end
-
-  def update
-    @order.update(order_params)
-    respond_with(@order)
-  end
-
-  def destroy
-    @order.destroy
     respond_with(@order)
   end
 
