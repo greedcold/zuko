8a34c8b7d3a3a41f419deb34b08d3c3da2ee65d7
Ant <alonezuko@mail.ru>
2015-01-31 23:25:12 +0300

    try fix connecting orders and users

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,11 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
+  def index
+    @orders = Order.all
+    respond_with(@orders)
+  end
+
   def sales
     @orders = Order.all.where(seller: current_user).order("created_at DESC")
   end
@@ -13,13 +18,13 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
-    respond_with(@order)
   end
 
   def create
     @order = Order.new(order_params)
     @listing = Listing.find(params[:listing_id])
     @seller = @listing.user
+
     @order.listing_id = @listing.id
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
@@ -28,6 +33,8 @@ class OrdersController < ApplicationController
     @order.save
     respond_with(@order)
   end
+
+
 
   private
     def set_order
